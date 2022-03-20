4708daabd75a83dc22363c35e96fbea7ba256b6f
Daniel Truong <aduy1122@gmail.com>
2022-03-20 13:58:32 -0700

    Changing variable name so it's more readable

--- scripts/main.go
+++ scripts/main.go
@@ -9,9 +9,10 @@ import (
 )
 
 func main() {
-	file, err := os.Open("words.txt")
-	oFile, oErr := os.OpenFile("../src/words.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	inputFile, err := os.Open("words.txt")
+	outputFile, oErr := os.OpenFile("../src/words.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	key := `"words":`
+
 	if oErr != nil {
 		log.Fatalf("Failed creating file %s", oErr)
 	}
@@ -19,30 +20,33 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed open file %s", err)
 	}
-
-	datawriter := bufio.NewWriter(oFile)
-	scanner := bufio.NewScanner(file)
+	//Read text file
+	datawriter := bufio.NewWriter(outputFile)
+	scanner := bufio.NewScanner(inputFile)
 	scanner.Split(bufio.ScanLines)
 	var txt []string
 	for scanner.Scan() {
 		txt = append(txt, scanner.Text())
 	}
-	var y []string
-	file.Close()
+	var json []string
+	inputFile.Close()
 
+	//Create json string
 	for i := 0; i < len(txt); i++ {
 		if i != 0 && i != len(txt)-1 {
-			y = append(y, `"`, txt[i], `", `)
+			json = append(json, `"`, txt[i], `", `)
 		} else if i == 0 {
-			y = append(y, "{ \n", key, "[", `"`, txt[i], `", `)
+			json = append(json, "{ \n", key, "[", `"`, txt[i], `", `)
 		} else if i == len(txt)-1 {
-			y = append(y, `"`, txt[i], `"`, "] \n", "}")
+			json = append(json, `"`, txt[i], `"`, "] \n", "}")
 		}
 	}
-	for _, v := range y {
+
+	//Output file
+	for _, v := range json {
 		_, _ = datawriter.WriteString(v)
 	}
 	datawriter.Flush()
-	oFile.Close()
+	outputFile.Close()
 
 }
